2c5a841f7b5940c5b9cf8bafd15d7c5fa39aa514
swordy <mrkardostamas@gmail.com>
2020-02-09 23:07:44 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,54 @@
+from flask import Flask, render_template
+import sqlite3
+
+app = Flask(__name__)
+
+
+def dict_factory(cursor, row):
+    d = {}
+    for idx, col in enumerate(cursor.description):
+        d[col[0]] = row[idx]
+    return d
+
+
+@app.route('/')
+def main():
+    with sqlite3.connect("db.db") as conn:
+        conn.row_factory = dict_factory
+        c = conn.cursor()
+
+        c.execute("SELECT * FROM interviews")
+        datas = c.fetchall()
+
+        out = {}
+        for data in datas:
+            tags_str = data["tags"]
+            tags = tags_str.split(';')
+            for tag in tags:
+                if tag in out:
+                    out[tag] += [data]
+                else:
+                    out[tag] = [data]
+    return render_template("main.html", groups=out.keys())
+
+
+@app.route('/<group>')
+def mobil(group):
+    with sqlite3.connect("db.db") as conn:
+        conn.row_factory = dict_factory
+        c = conn.cursor()
+
+        c.execute("SELECT * FROM interviews")
+        datas = c.fetchall()
+
+        out = {}
+        for data in datas:
+            tags_str = data["tags"]
+            tags = tags_str.split(';')
+            for tag in tags:
+                if tag in out:
+                    out[tag] += [data]
+                else:
+                    out[tag] = [data]
+    return render_template("cards.html", groups=out.keys(), datas=out[group], selected_group=group)
+
