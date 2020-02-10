bec18ffd1dec53ab672c95317f0d12efd89d8aab
swordy <mrkardostamas@gmail.com>
2020-02-10 16:32:10 +0100

    Move app files to app folder

---
+++ start.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+app="cppcards"
+docker build -t ${app} .
+
+docker run -d --name ${app} \
+   --expose 80 --net nginx-proxy -e VIRTUAL_HOST=cppcards.swordey.duckdns.org \
+   -v $PWD:/app ${app}
