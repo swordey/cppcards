68b24779b26d7896614f8c03a2620dfacd93b775
swordy <mrkardostamas@gmail.com>
2020-02-15 12:01:11 +0100

    remove start.sh

--- start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-app="cppcards"
-docker build -t ${app} .
-
-docker run -d --name ${app} \
-   --expose 80 --net nginx-proxy \
-   -e VIRTUAL_HOST=cppcards.swordey.duckdns.org \
-   -v $PWD:/app ${app}
